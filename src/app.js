cb342d4b555729b090524e9fbcf7b5d652898964
Yojan Stiben  Hungria <yshungria.4geeks@gmail.com>
2023-03-14 02:27:21 +0000

    Domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "their"];
+
+  let adj = ["great", "big", "small"];
+
+  let noun = ["jogger", "racoon", "elephant"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[index] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
